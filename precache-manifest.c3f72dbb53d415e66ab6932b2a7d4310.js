eba857730e2a72dd536e94c7f2e495b465179b63
Anton Finnson <finnsonanton@gmail.com>
2022-05-27 17:16:19 +0200

    Updates

---
+++ precache-manifest.c3f72dbb53d415e66ab6932b2a7d4310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87d908e1c76fdb61ea6",
+    "url": "/happybirthday/static/css/main.6cad8a6b.chunk.css"
+  },
+  {
+    "revision": "c87d908e1c76fdb61ea6",
+    "url": "/happybirthday/static/js/main.c87d908e.chunk.js"
+  },
+  {
+    "revision": "ba7e7644c94d9ae4a8d4",
+    "url": "/happybirthday/static/js/1.ba7e7644.chunk.js"
+  },
+  {
+    "revision": "95e0bcfb566caf15613a",
+    "url": "/happybirthday/static/js/runtime~main.95e0bcfb.js"
+  },
+  {
+    "revision": "caa9f3c3ddd92d42bb4fcce67152fc76",
+    "url": "/happybirthday/static/media/fanfare.caa9f3c3.mp3"
+  },
+  {
+    "revision": "0357b49a9f5a723dbdcaa684cd519fba",
+    "url": "/happybirthday/static/media/balloons_375.0357b49a.jpg"
+  },
+  {
+    "revision": "487bd23654dc51f6da0d883cc623924e",
+    "url": "/happybirthday/index.html"
+  }
+];
